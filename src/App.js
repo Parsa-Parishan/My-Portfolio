ed9f42ba15a984cdadd5fa89e1a122c54c5664d3
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-14 18:50:14 -0400

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <header>
+        <Header />
+      </header>
     </div>
   );
 }
