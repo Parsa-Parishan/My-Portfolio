94a4d375defbb26acff3eaa82375ade6594f6e85
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-15 14:57:08 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div class="social-media">
+        <ul>
+          <li class="media">
+            <a href="#">
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </li>
+          <li class="media">
+            <a href="#">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+          <li class="media">
+            <a href="#">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+          </li>
+          <li class="media">
+            <a href="#">
+              <i class="fa-brands fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="credit">
+        <a href="#">Designed & Built by Parsa Parishan</a>
+      </div>
+    </>
+  );
+}
